8f54c5278cde1991dafb10841a7942a416bc1369
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-01-15 18:36:56 +0100

    reformt calls + classes

--- src/playGround/PlayGround.py
+++ src/playGround/PlayGround.py
@@ -31,14 +31,4 @@ class Dog(Animal):
         return self.race
 
 
-wale = Animal("Rüdiger", "4", 5)
-wale.introduceMyself()
-wale.age = 1
-wale.countMyAge()
 
-soki = Dog("Sokrates", "4", 13, "Terrier")
-soki.introduceMyself()
-sokisRace = soki.tellYourRace()
-print("My Race? I'm a {}!".format(sokisRace))
-soki.age = 4
-soki.countMyAge()
