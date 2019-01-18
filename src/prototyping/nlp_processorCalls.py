22665d9a2c2f9eb65bd7c8eab80eb9905c287a30
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-01-18 19:12:06 +0100

    added word and sentence tokenization

--- src/prototyping/nlp_processorCalls.py
+++ src/prototyping/nlp_processorCalls.py
@@ -1,24 +1,36 @@
 
 from src.prototyping.NlpProcessor import NlpProcessor
 
-#from cltk.tokenize.sentence import TokenizeSentence
+from cltk.tokenize.sentence import TokenizeSentence
+from cltk.corpus.utils.importer import CorpusImporter
+from cltk.tokenize.word import nltk_tokenize_words
 
 # test run ...> everything goes as expected
-dir = "C:/Users/stoffse/PycharmProjects/CantusNlp/resources/sampledata/"
+dir = "C:/Users/Sebi/PycharmProjects/cantusNlp-py.git/resources/sampleData/"
 processor = NlpProcessor(dir)
 processor.loadLocalXml()
 
 # test if everything is inside
-for x in processor.textMap:
-    print(x)
-    print(processor.textMap[x])
+# for x in processor.textMap:
+#     print(x)
+#     print(processor.textMap[x])
 
 
 
-##########NLP TRIES#####
+############################
+##########NLP Procedure#####
+############################
 
-#tokenizer = TokenizeSentence("latin")
+# i need to import the datamodels first! ...> run this first (don't need to repeat!)
+#my_latin_downloader = CorpusImporter('latin')
+#my_latin_downloader.import_corpus('latin_text_latin_library')
+#my_latin_downloader.import_corpus('latin_models_cltk')
 
-#myTokens = tokenizer.tokenize_sentences(processor.textMap["fragment_1a.xml"])
+txt = processor.textMap["fragment_1a.xml"]
 
-#print(myTokens)
+sentenceTokenizer = TokenizeSentence('latin')
+sentences = sentenceTokenizer.tokenize_sentences(txt)
+print(sentences)
+
+wordTokens = nltk_tokenize_words(txt)
+print(wordTokens)
