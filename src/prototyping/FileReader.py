b16e447b31230bfcb545e24f701f079c98bdec62
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-01-18 11:38:03 +0100

    started with the file reader

--- src/prototyping/FileReader.py
+++ src/prototyping/FileReader.py
@@ -1,3 +1,16 @@
 
+import os
 
 # in here the file / directory loop operations etc.
+
+class FileReader:
+
+    path: str = ""
+
+    def __init__(self,path):
+        self.path = path
+
+
+    def listFiles(self):
+        return os.listdir(self.path)
+
