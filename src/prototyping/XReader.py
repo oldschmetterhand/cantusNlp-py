e286034e8ec6257f417543a1f6a23bc895a8e98f
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-01-18 10:12:37 +0100

    playing around reading xml

--- src/prototyping/XReader.py
+++ src/prototyping/XReader.py
@@ -19,11 +19,27 @@ reader = XReader()
 xml = reader.readXml("test01.xml")
 print(xml)
 
+#getRoot elem and print one childElemsText
 root = reader.getRoot(xml)
 print(root[0][1].text)
 
-# looping through tags
-allNodes = root.iter()
+#get complete text via loop and concatination
+allElems = root.findall(".//")
+print(allElems)
+concat = ''
+for elem in allElems:
+    print(elem.text)
+    if elem.text is not None:
+        concat += elem.text
 
-tree = ElementTree.parse('test01.xml')
-print(ElementTree.tostring(tree, encoding='utf-8', method='text'))
+
+print(concat)
+
+
+# looping through tags
+#allNodes = root.iter()
+
+#tree = ElementTree.parse('test01.xml')
+#print(ElementTree.tostring(tree, encoding='utf-8', method='text'))
+
+
