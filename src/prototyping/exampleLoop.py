f5729bb1fb63165596d2b194106fa487d4e3b4dd
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-01-21 13:16:59 +0100

    example looping through the different words xmls in text using the processor  ...> aggregating to one corpus (string) ...> there are some errors (see try / except) etc.

---
+++ src/prototyping/exampleLoop.py
@@ -0,0 +1,35 @@
+from src.prototyping.NlpProcessor import NlpProcessor
+
+dir = "C:/Users/stoffse/PycharmProjects/CantusNlp/resources/sampledata/"
+processor = NlpProcessor(dir)
+processor.loadCorpus()
+processor.retrieveLatinModels() # ...> loads the dataModels from the internet
+
+map = processor.getTextMap()
+
+
+aggregated_str = ""
+
+for key in map:
+    print("loop")
+
+    cur_txt = processor.getText(key)
+    tokenized_txt = processor.wtokenizeWords(cur_txt)
+    try:
+        lemmatized_txt = processor.lemmatizeLat(tokenized_txt)
+        lemmatized_noStops = processor.removeLatStopWords(tokenized_txt)
+        for word in lemmatized_noStops:
+            aggregated_str += word + " "
+
+    except:
+        print("err in: " + key)
+
+
+
+
+
+
+
+
+
+print(aggregated_str)
