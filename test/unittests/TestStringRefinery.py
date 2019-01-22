91a89796440f3432adbca6f4cff05a4108712aed
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-01-22 15:33:37 +0100

    added more tests ...> for TDD

--- test/unittests/TestStringRefinery.py
+++ test/unittests/TestStringRefinery.py
@@ -16,3 +16,21 @@ class Test_refineElemText(unittest.TestCase):
         act = strRefiner.refineElemTxt(sample)
         self.assertEqual(act, exp)
 
+
+    def test_cleansPollutedString_withoutNumbers(self):
+        sample = "              rabbit      []          \n\n \t             bambus \n                   "
+        exp = "rabbit bambus"
+        act = strRefiner.refineElemTxt(sample)
+        self.assertEqual(exp, act)
+
+    def test_removesArabicNumbers(self):
+        sample = "          parrot99999dog91812338luce  "
+        exp = "parrot dog luce"
+        act = strRefiner.refineElemTxt(sample)
+        self.assertEqual(exp, act)
+
+    def test_cleansHighlyPollutedString_withNumbers(self):
+        sample = "  2     312       rabbit      []          \n\n \t            bambus \n           934917394123        "
+        exp = "rabbit bambus"
+        act = strRefiner.refineElemTxt(sample)
+        self.assertEqual(exp, act)
