484cef25326266759f6eff4de011ce4a0d5cb142
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-01-22 17:27:57 +0100

    moved methods

--- src/cantusNlp/NlpProcessor.py
+++ src/cantusNlp/NlpProcessor.py
@@ -8,6 +8,7 @@ from collections import Counter
 from cltk.stop.latin import STOPS_LIST
 from cltk.stem.lemma import LemmaReplacer
 
+
 class NlpProcessor:
 
     _dirPath: str
@@ -53,43 +54,6 @@ class NlpProcessor:
         """
         corpus: str = self._textMap[fileName]
         return corpus
-
-    def retrieveLatinModels(self):
-        """
-        Loads the required Latin data models (for the cltk processing) from the internet.
-        Uses the CorpusImporter('latin') to access the resources.
-        The data will be stored in the local project ...> from then the cltk
-        """
-        latinDownloader = CorpusImporter('latin')
-        latinDownloader.import_corpus('latin_text_latin_library')
-        latinDownloader.import_corpus('latin_models_cltk')
-
-    def wtokenizeLatin(self, text: str):
-        """
-        Uses the latin word tokenizer from cltk to tokenize the words for given text.
-        Removes punctuation internally.
-        :param text: Text to tokenize.
-        :return: List comprehension of tokenized words.
-        """
-        text = text.lower()
-        #print(text)
-        wordTokenizer = WordTokenizer("latin")
-        tokens = wordTokenizer.tokenize(text)
-        tokens: list = self._removePunctuation(tokens)
-        return tokens
-
-    def countWords(self, text: list):
-        wordsDict: dict = Counter(text)
-        return wordsDict
-
-    def removeLatStopWords(self, tokenized_text: list):
-        removedStops: list[str] = [w for w in tokenized_text if not w in STOPS_LIST]
-        return removedStops
-
-    def lemmatizeLat(self, tokenized_words: list):
-        lemmatizer = LemmaReplacer('latin')
-        lemmata: list = lemmatizer.lemmatize(tokenized_words)
-        return lemmata
 
     def getTextMap(self):
         return self._textMap
