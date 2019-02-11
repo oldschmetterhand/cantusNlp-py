970d6bafe80d1d3e7ecacbb36592870603859fae
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-02-11 16:16:04 +0100

    refactored to fit new method calls

--- src/cantusNlp/Main.py
+++ src/cantusNlp/Main.py
@@ -6,7 +6,7 @@ from src.cantusNlp.utils.CltkOperator import CltkOperator
 
 fr = FileReader()
 projDirectory = fr.calcPath(__file__).parent.parent.parent
-dataDir = str(projDirectory) + "/resources/cantus/"
+dataDir = str(projDirectory) + "/resources/txtData/"
 
 # start nlp
 nlp = NlpProcessor(dataDir)
@@ -22,48 +22,10 @@ for key in map:
     text = nlp._strRefiner.refineElemTxt(text)
     text = cltk.wtokenizeLatin(text)
     # text = cltk.lemmatizeLat(text)
-    text = cltk.removeLatStopWords(text)
+    # text = cltk.removeLatStopWords(text)
     map[key] = text
 
+cltk.displayCltkLemmaDeviation(map["txt.txt"])
 
-array = []
-
-
-
-for key in LEMMATA:
-    key = str.lower(key)
-    array.append(key)
-    # print(key)
-
-
-# print(array)
-
-for word in map["TEI_SOURCE.xml"]:
-
-    if word not in array:
-        print(word)
-
-
-
-# print(map)
-
-
-# print(nlp.getText("fragment_07.xml"))
-
-# TODO move following code inside NlpPRocessor ...> should not have to loop by hand.
-
-# testprints
-# map = nlp.getTextMap()
-# print(map)
-
-# aggr = " "
-#
-# for key in map:
-#     curentry = map[key]
-#
-#     for word in curentry:
-#         aggr += word + " "
-#
-#
-#
-# print(aggr)
+aggregStr = nlp.aggregateWordlist(map["txt.txt"])
+print(aggregStr)
