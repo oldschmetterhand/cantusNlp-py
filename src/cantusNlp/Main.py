bf32a776f34541f60ff3bc19a8d60593a4590c87
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-06-01 16:53:53 +0200

    commented out test - call of the map

--- src/cantusNlp/Main.py
+++ src/cantusNlp/Main.py
@@ -5,7 +5,7 @@ from src.cantusNlp.utils.FileReader import FileReader
 # first setting up data and result path for the analysis
 fr = FileReader()
 projDirectory = fr.calcPath(__file__).parent.parent.parent
-dataDir = str(projDirectory) + "/resources/txtData"
+dataDir = str(projDirectory) + "/resources/try_01"
 resultDir = str(projDirectory) + "/resources/analyzis"
 
 # then start nlp process
@@ -13,3 +13,8 @@ nlp = NlpProcessor(dataDir, resultDir)
 nlp.loadCorpus("note")
 nlp.lemmatizeCorpus(True)
 # nlp.lemmatizeCorpus()
+
+# print(str(nlp._nlpResultMap.get_result("Brixen.txt").get_list_of_lemma()))
+
+# for lemma in nlp._nlpResultMap.get_result("Brixen.txt").get_list_of_lemma():
+    # print(str(lemma))
