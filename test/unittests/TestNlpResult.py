40a12a7ce35d810f979ab2383f83affd3506814d
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-06-01 16:04:56 +0200

    added test for reading in the cltk list

--- test/unittests/TestNlpResult.py
+++ test/unittests/TestNlpResult.py
@@ -8,9 +8,9 @@ projectDir = str(fr.calcPath(__file__).parent.parent.parent)
 
 mock_lemma01 = Lemma("Pferd", "Pferde", 21)
 mock_lemma02 = Lemma("Haus", "Häuser", 22)
-
 mock_lemma_list = [mock_lemma01, mock_lemma02]
 
+mock_cltk_lemma_list = ["Pferd/Pferde", "Haus/Häuser", "Flamingo/Flamingos"]
 
 class Test_instantiation(unittest.TestCase):
 
@@ -22,9 +22,23 @@ class Test_instantiation(unittest.TestCase):
 class Test_return_array_of_lemma_dicts(unittest.TestCase):
 
     def test_returned_array_has_length_of_2(self):
-        res = res = NlpResult(mock_lemma_list)
+        res = NlpResult(mock_lemma_list)
         act_length = len(res.return_array_of_lemmas_dicts())
         exp_length = 2
         self.assertEquals(exp_length, act_length)
 
-        print(res.return_array_of_lemmas_dicts())
+    def test_throws_when_list_empty(self):
+        res = NlpResult()
+        self.assertRaises(ValueError, res.return_array_of_lemmas_dicts)
+
+
+class Test_add_cltk_lemma_list(unittest.TestCase):
+
+    def test_lemma_list_has_expected_length(self):
+        res = NlpResult()
+        res.add_cltk_lemma_list(mock_cltk_lemma_list)
+
+        act_length = len(res.get_list_of_lemma())
+        exp_length = 3
+
+        self.assertEquals(act_length, exp_length)
