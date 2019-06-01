9d2ec827138f213905d87d70213cc7828cf88720
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-06-01 13:57:34 +0200

    added base Lemma class

---
+++ src/cantusNlp/utils/nlpPhenomena/Lemma.py
@@ -0,0 +1,12 @@
+
+
+class Lemma:
+
+    def __init__(self, lemma: str, source: str, word_position: int):
+
+        self._lemma: str = lemma
+        self._source: str = source
+        self._word_position = word_position
+
+
+
