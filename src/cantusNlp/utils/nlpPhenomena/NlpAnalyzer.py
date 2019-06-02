fd7921edaa8acfeea851a93fe0066fc410442f8c
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2019-06-02 15:35:04 +0200

    now also assigning key to the most_occurent_lemma - dictionary

--- src/cantusNlp/utils/nlpPhenomena/NlpAnalyzer.py
+++ src/cantusNlp/utils/nlpPhenomena/NlpAnalyzer.py
@@ -11,9 +11,11 @@ class NlpAnalyser:
     def __init__(self, nlp_result_map: NlpResultMap):
         self._nlp_result_map = nlp_result_map
 
-    def calculate_most_occurence_lemmata(self, min_lemma_occurence: int) -> List[Dict[str, str or int]]:
-        keys: list = self._nlp_result_map._result_map.keys()
+    def calculate_most_occurence_lemmata(self, min_lemma_occurence: int) -> Dict[str, List[Dict[str, str or int]]]:
 
+        corpus_occurent_lemma: Dict[str, List[Dict[str, str or int]]] = {}
+
+        keys: list = self._nlp_result_map._result_map.keys()
         all_lemma: List[str] = []
 
         for key in keys:
@@ -23,21 +25,22 @@ class NlpAnalyser:
             for lemma in lemma_dicts:
                 all_lemma.append(lemma.get("lemma"))
 
-        uniques: set = set(all_lemma)
-        lemma_uniques: list = []
+            uniques: set = set(all_lemma)
+            lemma_uniques: list = []
 
-        for unique in uniques:
-            if unique in all_lemma:
-                lemma_count = all_lemma.count(unique)
+            for unique in uniques:
+                if unique in all_lemma:
+                    lemma_count = all_lemma.count(unique)
 
-                if lemma_count >= min_lemma_occurence:
-                    lemma_uniques.append({
-                        'name': str(unique),
-                        'value': lemma_count
-                    })
+                    if lemma_count >= min_lemma_occurence:
+                        lemma_uniques.append({
+                            'name': str(unique),
+                            'value': lemma_count
+                        })
 
-        print(str(lemma_uniques))
-        return lemma_uniques
+            # print(str(lemma_uniques))
+            corpus_occurent_lemma[key] = lemma_uniques
 
-            # self.write_dict_to_json(dict_to_write, str.replace(key, ".", "_") + "/lemmatizationResult.json")
+        print(corpus_occurent_lemma)
+        return corpus_occurent_lemma
 
